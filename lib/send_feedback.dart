b8e7a17042c1c3a740aa5b08b35c9202fa7f8707
vivizsi <144732898+vivizsi@users.noreply.github.com>
2024-04-10 09:09:13 -0600

    Create send_feedback.dart

---
+++ lib/send_feedback.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SendFeedbackPage extends StatefulWidget {
+  @override
+  _SendFeedbackPageState createState() => _SendFeedbackPageState();
+}
+
+class _SendFeedbackPageState extends State<SendFeedbackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(
+            child: Container(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [
+                  Color(0xFFFBA3660),
+                  Color(0xFF1C256E),
+                ],
+                stops: [0.3, 0.75],
+              ),
+            ),
+          ),
+        )),
+      ),
+    );
+  }
+}
